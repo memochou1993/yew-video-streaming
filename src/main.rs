387fe5bc8f64a69871599f7b6d8b0acfd4bd158b
Memo Chou <memochou1993@gmail.com>
2022-09-25 21:07:53 +0800

    Add style

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,33 @@
 use yew::prelude::*;
 
+#[function_component(Producer)]
+fn producer() -> Html {
+    html!(
+        <div class="producer">
+            <h3>
+                {"Producer"}
+            </h3>
+        </div>
+    )
+}
+
+#[function_component(Consumer)]
+fn consumer() -> Html {
+    html!(
+        <div class="consumer">
+            <h3>
+                {"Consumer"}
+            </h3>
+        </div>
+    )
+}
+
 #[function_component(App)]
 fn app() -> Html {
     html!(
-        <div>
-            {"Hello, World!"}
+        <div class={"grid"}>
+            <Producer />
+            <Consumer />
         </div>
     )
 }
